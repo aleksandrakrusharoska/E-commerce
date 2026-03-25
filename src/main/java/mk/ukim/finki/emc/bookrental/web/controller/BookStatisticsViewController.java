7dbedc83d56b65f5b55c40f870e59b399769b6f9
aleksandrakrusharoska <aleksandrakrusharoska@gmail.com>
2026-03-25 10:12:11 +0100

    Add new files

--- src/main/java/mk/ukim/finki/emc/bookrental/web/controller/BookStatisticsViewController.java
+++ src/main/java/mk/ukim/finki/emc/bookrental/web/controller/BookStatisticsViewController.java
@@ -1,33 +1,32 @@
-//package mk.ukim.finki.emc.bookrental.web.controller;
-//
-//import lombok.RequiredArgsConstructor;
-//import mk.ukim.finki.emc.bookrental.model.dto.DisplayBookStatisticsDto;
-//import mk.ukim.finki.emc.bookrental.service.application.BookStatisticsViewApplicationService;
-//import mk.ukim.finki.emc.bookrental.service.domain.MaterializedViewRefreshService;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping("/api/book-statistics")
-//@RequiredArgsConstructor
-//public class BookStatisticsViewController {
-//
-//    private final BookStatisticsViewApplicationService service;
+package mk.ukim.finki.emc.bookrental.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import mk.ukim.finki.emc.bookrental.model.dto.DisplayBookStatisticsDto;
+import mk.ukim.finki.emc.bookrental.service.application.BookStatisticsViewApplicationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/book-statistics")
+@RequiredArgsConstructor
+public class BookStatisticsViewController {
+
+    private final BookStatisticsViewApplicationService service;
 //    private final MaterializedViewRefreshService refreshService;
-//
-//    @GetMapping
-//    public ResponseEntity<List<DisplayBookStatisticsDto>> findAll() {
-//        return ResponseEntity.ok(service.findAll());
-//    }
-//
+
+    @GetMapping
+    public ResponseEntity<List<DisplayBookStatisticsDto>> findAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
 //    @PostMapping("/refresh")
 //    public ResponseEntity<Void> refresh() {
 //        refreshService.refreshBookStatistics();
 //        return ResponseEntity.ok().build();
 //    }
-//}
+}
